0a85ff6346b952ac42cea4b381e4ef2b7073cd61
Nguyen <e6256894@cmaisonneuve.qc.ca>
2024-05-21 11:36:12 -0400

    Second commit: Commit final

--- Exercice1.py
+++ Exercice1.py
@@ -1,19 +1,11 @@
 import csv
 import json
 
-data_list = [[2, 3], [3, 2], [1.0, -5.3]]
-
-with open("data.json", "w") as data_json:
-    json.dump(data_list, data_json, indent=4)
-
 with open("data.json", 'r') as data_json:
-    tuples_list = json.load(data_json)
+    complexe_liste = json.load(data_json)
 
-with open("data.csv", "w") as data_csv:
+with open("data.csv", "w", newline='') as data_csv:
     data_csv_writer = csv.writer(data_csv, delimiter=",")
     data_csv_writer.writerow(["reel", "imaginaire"])
-    for tuple in tuples_list:
-        data_csv_writer.writerow([tuple[0], tuple[1]])
-
-
-
+    for nombre in complexe_liste:
+        data_csv_writer.writerow([nombre[0], nombre[1]])
--- Exercice2.py
+++ Exercice2.py
@@ -6,16 +6,12 @@ def charger_pokemon_csv(csv_file):
         csv_reader = csv.reader(csv_file, delimiter=",")
         compteur = 0
         for row in csv_reader:
-            if compteur == 0:
-                pass
-                compteur += 1
-            else:
-                dictionnary[row[0]] = [int(row[1]), int(row[2]), int(row[3]), int(row[4]), int(row[5]), int(row[6])]
-                compteur += 1
+            string_list = [row[1], row[2], row[3], row[4], row[5], row[6]]
+            dictionnary[row[0]] = [int(stat) for stat in string_list]
+            compteur += 1
     return dictionnary
 
 pkmn = charger_pokemon_csv("pokemon.csv")
-
 for nom, stats in pkmn.items():
     print(f"{nom}: {stats}")
 
